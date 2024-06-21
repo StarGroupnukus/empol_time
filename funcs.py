fdc2e15eae6fc7a098d8bbb0f7d512bea73b2cca
StarGroup <statgroupnukus@gmail.com>
2024-06-21 11:47:03 +0500

    search 5 neighbors,some changes

--- funcs.py
+++ funcs.py
@@ -8,157 +8,90 @@ from pymongo import MongoClient
 
 load_dotenv()
 
+
 def setup_logger(name, log_file, level=logging.DEBUG):
     """Настройка и создание логгера с заданными параметрами."""
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-    # Файловый обработчик с фильтром
     file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
     file_handler.setLevel(level)
 
-    # Консольный обработчик
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
     console_handler.setLevel(logging.INFO)
 
-    # Создаем и настраиваем логгер
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)  # Уровень DEBUG для сбора всех сообщений
+    logger.setLevel(logging.DEBUG)
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
     return logger
+
 
 os.makedirs('logs', exist_ok=True)
 logger = setup_logger("flog", "logs/flog.log")
 
 
 def extract_date_from_filename(filename):
-    """
-    Извлекает дату из имени файла.
-
-    Args:
-    filename (str): Имя файла.
-
-    Returns:
-    str: Дата в формате YYYYMMDD.
-    """
+    """Извлекает дату из имени файла."""
     try:
-        date_str = filename.split("_")[
-            2
-        ]  # Предполагается, что дата находится в третьей части имени файла
-        date_obj = datetime.strptime(date_str, "%Y%m%d%H%M%S%f")
-        return date_obj
+        date_str = filename.split("_")[2]
+        return datetime.strptime(date_str, "%Y%m%d%H%M%S%f")
     except Exception as e:
-        print(f"Произошла ошибка при извлечении даты из имени файла: {e}")
+        logger.error(f"Произошла ошибка при извлечении даты из имени файла: {e}")
         return None
 
 
-
 def send_report(camera_id, person_id, file_path, time, score, logger=logger):
-    file_name = file_path.split("/")[-1]
-    folder = f'{os.getenv("USERS_FOLDER_PATH")}/{person_id}/attendances'
+    file_name = os.path.basename(file_path)
+    folder = os.path.join(os.getenv("USERS_FOLDER_PATH"), str(person_id), "attendances")
     os.makedirs(folder, exist_ok=True)
-    os.rename(file_path, f'{folder}/{file_name}')
+    os.rename(file_path, os.path.join(folder, file_name))
+
     url = os.getenv("REPORT_URL")
     token = os.getenv("TOKEN_FOR_API")
     data = {
         "user_id": str(person_id),
         "device_id": str(camera_id),
-        "images[]": [file_name, ],
+        "images[]": [file_name],
         "time": time.strftime("%H:%M:%S"),
         "score": str(score),
     }
-    print(data)
 
     try:
-        with requests.post(
-                url, data=data, headers={
-                    "Accept": "application/json",
-                    "Authorization": f"Bearer {token}"
-                },
-                timeout=10
-        ) as response:
-            logger.info(response.status_code)
-            logger.info(f"{person_id} -- {score}")
-            print(response.text)
-            if response.status_code != 201:
-
-                logger.error(f'{person_id} - }')
-                pass
-                # document = {
-                #     "camera_id": str(camera_id),
-                #     "image_id": str(image_id),
-                #     "score": str(score),
-                #     "time": time.strftime("%Y-%m-%d %H:%M:%S"),
-                #     "file_path": file_path,
-                #     "status_code": response.status_code,
-                #     "send_time": datetime.now(),
-                # }
-                #
-                # # Подключение к MongoDB
-                # client = MongoClient(os.getenv("MONGODB_LOCAL"))
-                # # Укажите имя базы данных и коллекции
-                # db = client[os.getenv("DB_NAME")]
-                # collection = db["send_report"]
-                # collection.insert_one(document)
-            else:
-                logger.info(f"{person_id} -- {score} sent {response.status_code}")
-            logger.warning(f"{person_id} -- {score}")
+        response = requests.post(url, data=data, headers={
+            "Accept": "application/json",
+            "Authorization": f"Bearer {token}"
+        }, timeout=10)
+        logger.info(f"{person_id} -- {score} sent {response.status_code}")
+        if response.status_code != 201:
+            logger.error(f"Error: {response.status_code} for {person_id}")
+        else:
+            logger.info(f"Report sent successfully for {person_id}")
     except Exception as e:
-        # document = {
-        #     "camera_id": str(camera_id),
-        #     "image_id": str(image_id),
-        #     "score": str(score),
-        #     "time": time.strftime("%Y-%m-%d %H:%M:%S"),
-        #     "file_path": file_path,
-        #     "send_time": datetime.now(),
-        # }
-        #
-        # # Подключение к MongoDB
-        # client = MongoClient(os.getenv("MONGODB_LOCAL"))
-        # # Укажите имя базы данных и коллекции
-        # db = client[os.getenv("DB_NAME")]
-        # collection = db["send_report"]
-        # collection.insert_one(document)
-        logger.error(e)
+        logger.error(f"Exception while sending report: {e}")
 
 
 def get_faces_data(faces):
+    """Возвращает данные о лице с максимальной площадью прямоугольника."""
     if not faces:
         return None
-
-    max_face = max(faces, key=lambda face: calculate_rectangle_area(face["bbox"]))
-    return max_face
+    return max(faces, key=lambda face: calculate_rectangle_area(face["bbox"]))
 
 
 def calculate_rectangle_area(bbox):
-    # Проверяем, что список bbox содержит четыре элемента
+    """Вычисляет площадь прямоугольника."""
     if len(bbox) != 4:
-        raise ValueError(
-            "bbox должен содержать четыре координаты: x_min, y_min, x_max, y_max"
-        )
+        raise ValueError("bbox должен содержать четыре координаты: x_min, y_min, x_max, y_max")
+    return (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
 
-    # Вычисляем ширину и высоту прямоугольника
-    width = bbox[2] - bbox[0]
-    height = bbox[3] - bbox[1]
-
-    # Вычисляем площадь прямоугольника
-    area = width * height
-
-    return area
 
 def compute_sim(feat1, feat2, logger=logger):
-    from numpy.linalg import norm
-
+    """Вычисляет косинусное сходство между двумя векторами."""
     try:
-        feat1 = feat1.ravel()
-        feat2 = feat2.ravel()
-        sim = np.dot(feat1, feat2) / (norm(feat1) * norm(feat2))
-        return sim
+        feat1, feat2 = feat1.ravel(), feat2.ravel()
+        return np.dot(feat1, feat2) / (np.linalg.norm(feat1) * np.linalg.norm(feat2))
     except Exception as e:
-        logger.error(e)
+        logger.error(f"Error computing similarity: {e}")
         return None
-
-
