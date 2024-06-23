63af9906a12cd8b2051e49c256c5ef15bb2db254
AL1EN <wipetmp1404@proton.me>
2024-06-23 18:55:30 +0500

    init clients db

--- test.py
+++ test.py
@@ -72,12 +72,8 @@ class MainRunner:
         face_data = self.app.get(image)[0]
 
         client_data = {
-            'score': 0,
             "person_id": 0,
-            "embedding": face_data.embedding,
-            "gender": face_data.gender,
-            "age": face_data.age,
-            "date": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            "embedding": face_data.embedding.tolist(),
         }
 
         self.clients_db.insert_one(client_data)
