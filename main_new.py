54884339d49d9fdaa0e64a13d8a50a9f69e1d624
meloman <melomanovmeloman@gmail.com>
2024-08-16 20:10:26 +0500

    rb

--- main_new.py
+++ main_new.py
@@ -315,7 +315,7 @@ class MainRunner:
         try:
             url = f'{os.getenv("ADD_IMAGE_TO_USER")}/{employee_id}/images'
             data = {'employee_id': employee_id}
-            files = {'file': (open(img_path, 'rb'), "multipart/form-data")}
+            files = {'file': open(img_path, "rb")}
             headers = {
                 "Accept": "application/json",
             }
